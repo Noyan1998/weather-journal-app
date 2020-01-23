71e860211ae5965b68aec0edcea5c943fd77bb7b
Noyan Alimov <noyan.alimov@gmail.com>
2020-01-23 23:11:25 +0000

    First step on this project
    Edited html file to make it look more appropriate.
    Wrote server.js file.
    Started app.js file until I got stuck.

--- website/app.js
+++ website/app.js
@@ -1,5 +1,12 @@
 /* Global Variables */
+let baseURL = 'https://openweathermap.org/'
+let apiKey = 'a2adda676ea3cad6bbeba93ebfcbba31';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.querySelector(#generate).addEventListener('click', performAction);
+function performAction(e) {
+  getWeather(baseURL, )
+}
